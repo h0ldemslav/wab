ff8e03d77338e9ebd6a6292690cd6224b52a10bd
Daniil Astapenko <astapenko.daniil.03@gmail.com>
2024-01-21 22:50:51 +0100

    Add auth token save (should be in previous commit)

--- auth_service/src/main.py
+++ auth_service/src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+import httpx
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import RedirectResponse
 from auth import setup_auth
 
@@ -6,18 +7,43 @@ app = FastAPI()
 
 oauth = setup_auth(app)
 oauth_data = {}
-api_gateway_url = "http://127.0.0.1:8000"
+auth_service_urls = {
+    "base": "http://127.0.0.1:8001",
+    "login": "http://127.0.0.1:8001/login",
+    "token": "http://127.0.0.1:8001/token"
+}
+api_gateway_urls = {
+    "base": "http://127.0.0.1:8000",
+    "home": "http://127.0.0.1:8000/home",
+    "process_token": "http://127.0.0.1:8000/process_token"
+}
 
 @app.get("/login")
 async def login(request: Request):
-    return await oauth.google.authorize_redirect(request, "http://127.0.0.1:8001/token")
+    return await oauth.google.authorize_redirect(request, auth_service_urls["token"])
 
-@app.get("/token")
+@app.get(
+    path="/token",
+    include_in_schema=False
+)
 async def token(request: Request):
     try:
-        oauth_data["token"] = await oauth.google.authorize_access_token(request)
-        redirect_url = f"{api_gateway_url}/home"
+        token_data = await oauth.google.authorize_access_token(request)
+        oauth_data.update(token_data)
+
+        redirect_url = api_gateway_urls["home"]
+
+        await send_token_to_api_gateway(token_data)
 
         return RedirectResponse(url=redirect_url)
     except Exception:
-        return RedirectResponse(url=api_gateway_url)
+        return RedirectResponse(url=api_gateway_urls["base"])
+
+async def send_token_to_api_gateway(access_token: str):
+    headers = { "Content-Type": "application/json" }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(api_gateway_urls["process_token"], json=access_token, headers=headers)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Failed to send token to API Gateway")
